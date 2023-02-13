4b4e5969c1801e91ebd16f95a4909d11c89c1cf8
DCalder98 <105236647+DCalder98@users.noreply.github.com>
2023-02-13 13:09:58 -0500

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-    "https://technotes-reh7.onrender.com/",
-    "https://technotes-reh7.onrender.com"
+    'https://technotes-reh7.onrender.com',
+    'https://technotes-reh7.onrender.com'
 ]
 
 module.exports = allowedOrigins
